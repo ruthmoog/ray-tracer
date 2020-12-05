20345e82ca36d5a7793e6f406ff8b05a75ba8b38
Ruth Baker <ruthmoog@gmail.com>
2020-12-05 13:04:50 +0000

    Begin the Tuple class

---
+++ src/vector/Tuple.java
@@ -0,0 +1,48 @@
+package vector;
+
+public class Tuple {
+
+    private double x;
+    private double y;
+    private double z;
+    private double w;
+
+    public Tuple(double x, double y, double z, double w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+
+    public static Tuple point(double x, double y, double z) {
+        return new Tuple(x, y, z, 1.0);
+    }
+
+    public static Tuple vector(double x, double y, double z) {
+        return new Tuple(x, y, z, 0.0);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public double getW() {
+        return w;
+    }
+
+    public boolean isPoint() {
+        return w == 1;
+    }
+
+    public boolean isVector() {
+        return w == 0;
+    }
+}
---
+++ src/vector/TupleTest.java
@@ -0,0 +1,68 @@
+package vector;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TupleTest {
+
+    @Test
+    public void testPointTuple() {
+
+        // Given
+        Tuple tuple = new Tuple(4.3, -4.2, 3.1, 1.0);
+        double expectX = 4.3;
+        double expectY = -4.2;
+        double expectZ = 3.1;
+        double expectW = 1.0;
+
+        // When
+        double actualX = tuple.getX();
+        double actualY = tuple.getY();
+        double actualZ = tuple.getZ();
+        double actualW = tuple.getW();
+        boolean isPoint = tuple.isPoint();
+        boolean isVector = tuple.isVector();
+
+        // Then
+        Assert.assertEquals(expectX, actualX,0);
+        Assert.assertEquals(expectY, actualY,0);
+        Assert.assertEquals(expectZ, actualZ,0);
+        Assert.assertEquals(expectW, actualW,0);
+        Assert.assertTrue(isPoint);
+        Assert.assertFalse(isVector);
+    }
+
+    @Test
+    public void testVectorTuple() {
+        // Given
+        Tuple tuple = new Tuple(4.3, -4.2, 3.1, 0.0);
+
+        // When
+        boolean isPoint = tuple.isPoint();
+        boolean isVector = tuple.isVector();
+
+        // Then
+        Assert.assertFalse(isPoint);
+        Assert.assertTrue(isVector);
+    }
+
+    @Test
+    public void testCreatePoint() {
+        // Given
+        Tuple tuple = Tuple.point(1,2,3);
+        // When
+        boolean isPoint = tuple.isPoint();
+        // Then
+        Assert.assertTrue(isPoint);
+    }
+
+    @Test
+    public void testCreateVector() {
+        // Given
+        Tuple tuple = Tuple.vector(1,2,3);
+        // When
+        boolean isVector = tuple.isVector();
+        // Then
+        Assert.assertTrue(isVector);
+    }
+}
