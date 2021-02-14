39c2770f4bfa8fe990b3506d533d1d731c674df5
Ruth Baker <ruthmoog@gmail.com>
2021-02-14 15:18:55 +0000

    Add colors

--- src/tuple/Color.java
+++ src/tuple/Color.java
@@ -22,4 +22,28 @@ public class Color {
     public double getRed() {
         return red;
     }
+
+    public Color add(Color color) {
+        double red = color.getRed() + this.red;
+        double green = color.getGreen() + this.green;
+        double blue = color.getBlue() + this.blue;
+
+        return new Color(red, green, blue);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Color)) {
+            return false;
+        }
+
+        Color other = (Color) obj;
+        return  this.red == other.red &&
+                this.green == other.green &&
+                this.blue == other.blue;
+    }
 }
--- src/tuple/ColorTest.java
+++ src/tuple/ColorTest.java
@@ -16,4 +16,18 @@ public class ColorTest {
         Assert.assertEquals(color.getBlue(), 1.7, 0);
     }
 
+    @Test
+    public void addingColors() {
+        // Given
+        Color color1 = new Color(0.9, 0.6, 0.75);
+        Color color2 = new Color(0.7, 0.1, 0.25);
+        Color expected = new Color(1.6, 0.7, 1.0);
+
+        // When
+        Color actual = color1.add(color2);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
