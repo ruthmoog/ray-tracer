1b507c6a9a0dea04d8efa8496f07f94163cd9868
Ruth Baker <ruthmoog@gmail.com>
2021-03-03 12:00:11 +0000

    Write colors to pixels in a canvas

--- src/tuple/Canvas.java
+++ src/tuple/Canvas.java
@@ -1,19 +1,35 @@
 package tuple;
 
 public class Canvas {
-    private double width;
-    private double height;
+    private int width;
+    private int height;
+    private Color[][] pixels;
 
-    public Canvas(double height, double width) {
+    public Canvas(int height, int width) {
         this.height = height;
         this.width = width;
+        // create a 2d array of colors default to 000
+        this.pixels = new Color[height][width];
+        for (int row = 0; row < this.pixels.length; row++) {
+            for (int column = 0; column < this.pixels[row].length; column++) {
+                this.pixels[row][column] = new Color(0,0,0);
+            }
+        }
     }
 
-    public double getWidth() {
+    public int getWidth() {
         return width;
     }
 
-    public double getHeight() {
+    public int getHeight() {
         return height;
+    }
+
+    public Color pixelAt(int x, int y) {
+        return pixels[x][y];
+    }
+
+    public void writePixel(int x, int y, Color color) {
+        pixels[x][y] = color;
     }
 }
--- src/tuple/CanvasTest.java
+++ src/tuple/CanvasTest.java
@@ -8,8 +8,8 @@ public class CanvasTest {
     @Test
     public void createCanvas() {
         // Given
-        double height = 10;
-        double width = 20;
+        int height = 10;
+        int width = 20;
 
         // When
         Canvas canvas = new Canvas(height, width);
@@ -17,5 +17,35 @@ public class CanvasTest {
         // Then
         Assert.assertEquals(height, canvas.getHeight(),0);
         Assert.assertEquals(width, canvas.getWidth(),0);
+    }
+
+    @Test
+    public void writePixelsToCanvas() {
+        // Given
+        Canvas canvas = new Canvas(10, 20);
+        Color red = new Color(1,0,0);
+        int x = 2;
+        int y = 3;
+
+        // When
+        canvas.writePixel(x, y, red);
+
+        // Then
+        Assert.assertEquals(red, canvas.pixelAt(x, y));
+    }
+
+    @Test
+    public void writeGREENPixelsToCanvas() {
+        // Given
+        Canvas canvas = new Canvas(10, 20);
+        Color red = new Color(0,1,0);
+        int x = 2;
+        int y = 3;
+
+        // When
+        canvas.writePixel(x, y, red);
+
+        // Then
+        Assert.assertEquals(red, canvas.pixelAt(x, y));
     }
 }
