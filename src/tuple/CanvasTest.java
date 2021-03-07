16f02f6e44dc075a18d3b7262d64babeecd38eff
Ruth Baker <ruthmoog@gmail.com>
2021-03-07 13:19:42 +0000

    Refactor test removes slime

--- src/tuple/CanvasTest.java
+++ src/tuple/CanvasTest.java
@@ -34,18 +34,4 @@ public class CanvasTest {
         Assert.assertEquals(red, canvas.pixelAt(x, y));
     }
 
-    @Test
-    public void writeGREENPixelsToCanvas() {
-        // Given
-        Canvas canvas = new Canvas(10, 20);
-        Color red = new Color(0,1,0);
-        int x = 2;
-        int y = 3;
-
-        // When
-        canvas.writePixel(x, y, red);
-
-        // Then
-        Assert.assertEquals(red, canvas.pixelAt(x, y));
-    }
 }
